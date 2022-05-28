77856aaabcd94d6bb16e7299f9138522c0ecee96
SitingWu <rosewu0901@gmail.com>
2022-05-28 18:57:06 +0200

    add Test System for Post.cpp and Point.cpp

---
+++ geometry2d/src/point_test.cpp
@@ -0,0 +1,21 @@
+#include "point.h"
+
+#include <cassert>
+#include <cstdlib>
+
+
+// LMT_SEL_BEGIN
+
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    /*Run all test functions*/
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
---
+++ geometry2d/src/pose_test.cpp
@@ -0,0 +1,28 @@
+#include "pose.h"
+
+#include <cassert>
+#include <cstdlib>
+
+// LMT_SEL_BEGIN
+
+
+
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    /*Run all test funcitons*/
+
+
+
+
+
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+
+}
